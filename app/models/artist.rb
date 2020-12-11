5f2f701b3aef8b295134f6a479b05ea77962cc82
okorkov <alex.okorkov@gmail.com>
2020-12-10 17:51:44 -0800

    commit tree e0f2d5b89ff4046b1725fa66596ccf0c929851c2

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,17 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    Genre.find(Song.all.first[:genre_id])
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    artist = Song.all.map {|song| song[:artist] == self}
+    artist.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    genre = Song.all.map {|song| song[:genre] == self}
+    genre.count
   end
 end
